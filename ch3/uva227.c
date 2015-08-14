1f39abdd276f36f8a3092ebbe99cad2696ec27e1
Jin ZHANG <jz.zhangjin@gmail.com>
2015-08-14 11:31:04 +0800

    Puzzle, ACM/ICPC World Finals 1993, UVa227

---
+++ ch3/uva227.c
@@ -0,0 +1,80 @@
+/*************************************************************************
+	> File Name: uav227.c
+	> Author: Jin ZHANG
+	> Mail: jz.zhangjin@gmail.com
+	> Created Time: 四  8/13 19:42:47 2015
+ ************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+
+#define maxn 5
+char cmd[1001];
+char a[maxn+1][maxn+1];
+
+void swap(char* x, char* y) {
+    char temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int main() {
+    int count = 0;
+
+    freopen("uva227.in", "r", stdin);
+
+    while (gets(a[0]) && a[0][0] != 'Z') {
+
+        // get input and find space
+        int sx = 0, sy = 0;
+        for (int i = 1; i < maxn; i++) {
+            gets(a[i]);
+            if (!a[i][maxn-1]) a[i][maxn-1] = ' ';
+        }
+        for (int i = 0; i < maxn; i++)
+            for (int j = 0; j < maxn; j++ )
+                if (a[i][j] == ' ') { sx = i; sy = j; break;}
+        
+        // go
+        char *c; int fail = 0;
+        memset(cmd, 0, sizeof(cmd));
+        while (scanf("%s\n", cmd + strlen(cmd)) && cmd[strlen(cmd)-1] != '0');
+
+        for (c = cmd; *c != '0'; c++) {
+            switch (*c) {
+                case 'L':
+                    if (sy == 0) fail = 1;
+                    else { swap(&a[sx][sy], &a[sx][sy-1]); sy--; }
+                    break;
+                case 'R':
+                    if (sy == maxn - 1) fail = 1;
+                    else { swap(&a[sx][sy], &a[sx][sy+1]); sy++; }
+                    break;
+                case 'A':
+                    if (sx == 0) fail = 1;
+                    else { swap(&a[sx][sy], &a[sx-1][sy]); sx--; }
+                    break;
+                case 'B':
+                    if (sx == maxn - 1) fail = 1;
+                    else { swap(&a[sx][sy], &a[sx+1][sy]); sx++; }
+                    break;
+            }
+            if (fail == 1) break;
+        }
+
+        // output
+        if (count++) printf("\n");
+        printf("Puzzle #%d:\n",count);
+        if (*c == '0') {
+            for (int i = 0; i < maxn; i++) {
+                for (int j = 0; j < maxn; j++) {
+                    printf ("%c", a[i][j]);
+                    j == maxn - 1 ? printf("\n") : printf(" ");
+                }
+            } 
+        } else {
+            printf("This puzzle has no final configuration.\n");
+        }
+    }
+    return 0;
+}
